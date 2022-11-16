59c9bbe3a7f907eefbf0b97197b4941391f88d18
RiverBythesea <riverdear.lxc@gmail.com>
2022-11-15 16:38:58 -0800

    Added menu toggle
    - added burger icon
    -edited margins and paddings
    added js for menu toggle

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,15 @@
 console.log ("Hi, welcome to my portfolio page!")
+var person = {
+  name: "River",
+  age: 24
+  hobbby: "reading"
+};
+console.log (person);
+
+function. menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  x.className += ' responsive';
+} else {
+  x.className = 'navtoggle'
+}
+}
