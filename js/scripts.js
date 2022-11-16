93cd63df3d097297c584d9afdedf8422ba6a7279
RiverBythesea <riverdear.lxc@gmail.com>
2022-11-15 22:03:45 -0800

    moved </a> in index and switched orders of CSS navtoggle code

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,3 @@
-console.log ("Hi, welcome to my portfolio page!")
-var person = {
-  name: "River",
-  age: 24
-  hobbby: "reading"
-};
-console.log (person);
-
 function. menuToggle() {
   var x = document.getElementById('myNavtoggle');
   x.className += ' responsive';
